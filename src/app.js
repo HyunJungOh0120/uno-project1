6755f508320c5761e308b2cedd3f4c356a042b1b
hyunjungagnes <hyunjung.agnes.oh@gmail.com>
2021-06-12 20:08:05 +0800

    Created Deck with 108 cards

--- src/app.js
+++ src/app.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const cardInfo = {
+  value: [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    'skip',
+    'reverse',
+    'draw2',
+    'wild',
+    'wildDraw4',
+  ],
+  color: ['red', 'yellow', 'green', 'blue'],
+  red: 'rgb(230,0,0)',
+  yellow: 'rgb(255,255,91)',
+  green: 'rgb(7,170,56)',
+  blue: 'rgb(0,35,230)',
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//// * GAME STATUS
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 const game = {
   players: ['user', 'pc1', 'pc2'],
   currPlayer: null,
@@ -9,6 +38,52 @@ const game = {
   currColor: null,
   currValue: null,
 };
+
+const playerHands = {
+  userHand: [],
+  pc1Hand: [],
+  pc2Hand: [],
+};
+
+const create1Set = () => {
+  const set = cardInfo.color.map((color) => {
+    const cards = [];
+    cardInfo.value.forEach((val) => {
+      const card = {
+        color: color,
+        faceUp: false,
+        value: val,
+        face: val,
+        color: color,
+      };
+      cards.push(card);
+    });
+    return cards;
+  });
+  return set;
+};
+
+const createDeck = () => {
+  const deck = [];
+  const set1 = create1Set().map((row) => row.slice(0, -1)); // Remove wildDraw 4 from set 1
+  const set2 = create1Set()
+    .map((row) => row.slice(1))
+    .map((row) => {
+      row.splice(-2, 1);
+      return row;
+    }); // Remove wild and blank 0 cards.
+  set1.forEach((row) => row.forEach((card) => deck.push(card)));
+  set2.forEach((row) => row.forEach((card) => deck.push(card)));
+  // Total 108 cards
+  return deck;
+};
+
+const deck = createDeck();
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//// * GAME
+/////////////////////////////////////////////////////////////////////////////////////////////
+const randomNum = (limit) => Math.floor(Math.random() * limit);
 
 const getNextIndex = () => {
   const nextIndex =
@@ -28,63 +103,9 @@ const changePlayer = () => {
   game.nextIndex = game.getNextIndex();
 };
 
-const card = {
-  value: [
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    'skip',
-    'reverse',
-    'draw2',
-    'wild',
-    'wildDraw4',
-  ],
-  suit: 8,
-  face: [
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    'skip',
-    'reverse',
-    'draw2',
-    'wild',
-    'wildDraw4',
-  ],
-  faceUp: false,
-  color: ['red', 'yellow', 'green', 'blue'],
-  red: 'rgb(230,0,0)',
-  yellow: 'rgb(255,255,91)',
-  green: 'rgb(7,170,56)',
-  blue: 'rgb(0,35,230)',
+const chooseTurn = () => {
+  randomNum();
 };
-
-const board = {};
-
-const playerHands = {
-  userHand: [],
-  pc1Hand: [],
-  pc2Hand: [],
-};
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-//// * GAME
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-const chooseTurn = () => {};
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 //// * MAIN
