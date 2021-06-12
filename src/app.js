9468d762f63fffa62ebd981874d074568a97a5b4
hyunjungagnes <hyunjung.agnes.oh@gmail.com>
2021-06-12 18:57:24 +0800

    Changed game, board objects (class => object)

--- src/app.js
+++ src/app.js
@@ -1,41 +1,78 @@
 'use strict';
 
-class Game {
-  constructor() {
-    this.players = ['user', 'pc1', 'pc2'];
-    this.currPlayer = 'user';
-    this.currIndex = this.players.indexOf(this.currPlayer);
-    this.nextIndex = this.getNextIndex();
-    this.currCard;
-    this.currColor;
-    this.currValue;
-  }
-  getNextIndex() {
-    const nextIndex =
-      this.currIndex === this.players.length - 1 ? 0 : this.currIndex + 1;
-    return nextIndex;
-  }
-  changePlayer() {
-    // Before Change
-    this.nextPlayer = this.players[this.nextIndex];
+const game = {
+  players: ['user', 'pc1', 'pc2'],
+  currPlayer: null,
+  currIndex: null,
+  nextIndex: null,
+  currCard: null,
+  currColor: null,
+  currValue: null,
+};
 
-    // Changing
-    this.currPlayer = this.nextPlayer;
-    this.currIndex = this.nextIndex;
+const getNextIndex = () => {
+  const nextIndex =
+    game.currIndex === game.players.length - 1 ? 0 : game.currIndex + 1;
+  return nextIndex;
+};
 
-    // After Change
-    this.nextIndex = this.getNextIndex();
-  }
-}
+const changePlayer = () => {
+  // Before Change
+  game.nextPlayer = game.players[getNextIndex()];
 
-class Board {
-  constructor() {
-    this.deck = this.createDeck();
-    this.drawPile = [];
-    this.discardPile = [];
-  }
-  createDeck() {}
-}
+  // Changing
+  game.currPlayer = game.nextPlayer;
+  game.currIndex = game.nextIndex;
+
+  // After Change
+  game.nextIndex = game.getNextIndex();
+};
+
+const card = {
+  value: [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    'skip',
+    'reverse',
+    'draw2',
+    'wild',
+    'wildDraw4',
+  ],
+  suit: 8,
+  face: [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    'skip',
+    'reverse',
+    'draw2',
+    'wild',
+    'wildDraw4',
+  ],
+  faceUp: false,
+  color: ['red', 'yellow', 'green', 'blue'],
+  red: 'rgb(230,0,0)',
+  yellow: 'rgb(255,255,91)',
+  green: 'rgb(7,170,56)',
+  blue: 'rgb(0,35,230)',
+};
+
+const board = {};
 
 const playerHands = {
   userHand: [],
